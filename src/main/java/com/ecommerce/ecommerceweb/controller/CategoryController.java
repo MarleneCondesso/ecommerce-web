bbad6811e9bb184b56ab5befaf18e9e757ddc044
MarleneCondesso <marlene.condesso23@gmail.com>
2023-07-07 13:33:18 +0100

    Clear some unused import statement

--- src/main/java/com/ecommerce/ecommerceweb/controller/CategoryController.java
+++ src/main/java/com/ecommerce/ecommerceweb/controller/CategoryController.java
@@ -6,12 +6,10 @@ import com.ecommerce.ecommerceweb.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/category")
